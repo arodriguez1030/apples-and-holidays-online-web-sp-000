35386e10d56ebafdee914e6abaa8601664be640a
Ashley Rodriguez <arodriguez1030@gmail.com>
2020-04-20 18:45:49 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,6 +59,9 @@ holiday_hash.each do |season, holiday|
     puts "#{season.to_s.capitalize!}:"
     data.each do |holiday, supply|
       str_holiday = holiday.to_s.split("_").collect {|word| word.capitalize!}.join(" ")
+      puts "  #{str_holiday}: #{supply.join(", ")}"
+    end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
